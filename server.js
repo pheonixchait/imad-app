4a4ca8adb7f3d4c9889c7680b36b20cb0b629108
phoenixchait <31011327+pheonixchait@users.noreply.github.com>
2017-09-01 08:59:19 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone=(
+    title: 
+    heading:
+    data:
+    content:
+);
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
