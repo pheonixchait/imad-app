1e72d32053d5d5f2e7e4827b7d9bd2ce6c98452b
phoenixchait <31011327+pheonixchait@users.noreply.github.com>
2017-09-01 10:12:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
- res.send(createTemplate(article-one));    
+ res.send(createTemplate(articles.article-one));    
 });
 
 app.get('/article-two', function (req, res) {
