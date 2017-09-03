f13c613e5d2b528308dd759f8a0bb36c1ff0a8d3
phoenixchait <31011327+pheonixchait@users.noreply.github.com>
2017-09-03 07:24:03 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 console.log('Loaded!');
+
+//changing content
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//moving image
+var img = document.getElementbyId('madi');
+
+img.onclick = function()
+{
+   img.style.marginleft = '100px';  
+};
